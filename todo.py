185780e914ed642878b21f67210dafdb7d2816af
Abhinandan Kumar <nandan645@protonmail.com>
2026-03-09 13:00:05 +0530

    simple bug fix

--- todo.py
+++ todo.py
@@ -1,9 +1,18 @@
 # todo.py
+
 tasks = []
 
 def add_task(task):
     tasks.append(task)
     print(f'Task "{task}" added.')
+
+def list_tasks():
+    if not tasks:
+        print("Task list is empty. Add some tasks!")
+    else:
+        print("\nYour Tasks:")
+        for idx, task in enumerate(tasks, start=1):
+            print(f'{idx}. {task}')
 
 def delete_task(index):
     if 0 <= index < len(tasks):
@@ -11,14 +20,8 @@ def delete_task(index):
         print(f'Task "{removed}" removed.')
     else:
         print("Invalid task number.")
-        
-def list_tasks():
-    if not tasks:
-        print("No tasks available.")
-    else:
-        for idx, task in enumerate(tasks, start=1):
-            print(f'{idx}. {task}')
 
 if __name__ == "__main__":
     add_task("Finish Assignment")
     list_tasks()
+    delete_task(0)
